8a1ddb4cfb64dc684ee4f1e275f440176deb0f82
Pratyush-who <pratyushmehra2005@gmail.com>
2025-03-06 02:02:28 +0530

    ui

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -4,6 +4,7 @@ import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -22,72 +23,188 @@ class _HomePageState extends State<HomePage> {
     id: '1',
     profileImage: 'assets/logo.png',
   );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: const Text('Gemini Chat'))),
-      body: DashChat(
-        inputOptions: InputOptions(
-          trailing: [
-            IconButton(
-              icon: Icon(Icons.image),
-              onPressed: () {
-                voidSendMediaMessage();
-              },
+      backgroundColor: const Color.fromRGBO(27, 27, 27, 1),
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(34, 38, 41, 1),
+        elevation: 0,
+        centerTitle: true,
+        leading: Image.asset('assets/logo.png', height: 24, width: 24),
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+           
+            const Text(
+              'Gemini Chat',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            ],
+          ],
         ),
-        currentUser: currentUser,
-        onSend: sendMessage,
-        messages: messages,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color.fromRGBO(27, 27, 27, 1),
+        ),
+        child: DashChat(
+          currentUser: currentUser,
+          onSend: sendMessage,
+          messages: messages,
+          inputOptions: InputOptions(
+            inputDecoration: InputDecoration(
+              hintText: "Ask Gemini something...",
+              hintStyle: const TextStyle(color: Colors.grey),
+              filled: true,
+              fillColor: const Color.fromRGBO(34, 38, 41, 0.7),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(24),
+                borderSide: BorderSide.none,
+              ),
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 10,
+              ),
+            ),
+            sendButtonBuilder: (onSend) {
+              return IconButton(
+                icon: const Icon(
+                  Icons.send_rounded,
+                  color: Color.fromRGBO(90, 216, 255, 1),
+                ),
+                onPressed: onSend,
+              );
+            },
+            trailing: [
+              IconButton(
+                icon: const Icon(
+                  Icons.image_rounded,
+                  color: Color.fromRGBO(90, 216, 255, 1),
+                ),
+                onPressed: () {
+                  voidSendMediaMessage();
+                },
+              ),
+            ],
+            inputToolbarStyle: BoxDecoration(
+              color: const Color.fromRGBO(34, 38, 41, 0.5),
+              borderRadius: BorderRadius.circular(0),
+            ),
+          ),
+          messageOptions: MessageOptions(
+            showTime: true,
+            timePadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+            timeFormat: DateFormat('HH:mm'),
+            messageDecorationBuilder: (message, previousMessage, nextMessage) {
+              bool isUser = message.user.id == currentUser.id;
+              return BoxDecoration(
+                color: isUser
+                    ? const Color.fromRGBO(90, 216, 255, 1)
+                    : const Color.fromRGBO(34, 38, 41, 1),
+                borderRadius: BorderRadius.circular(16),
+              );
+            },
+            messageTextBuilder: (message, previousMessage, nextMessage) {
+              bool isUser = message.user.id == currentUser.id;
+              return Text(
+                message.text,
+                style: TextStyle(
+                  color: isUser ? Colors.black : Colors.white,
+                  fontSize: 16,
+                ),
+              );
+            },
+            avatarBuilder: (user, onPress, longPress) {
+              if (user.id == geminiUser.id) {
+                return CircleAvatar(
+                  backgroundColor: const Color.fromRGBO(27, 27, 27, 1),
+                  child: ClipOval(
+                    child: Image.asset(
+                      'assets/logo.png',
+                      width: 40,
+                      height: 40,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              }
+              return CircleAvatar(
+                backgroundColor: const Color.fromRGBO(90, 216, 255, 1),
+                child: Text(
+                  user.firstName![0],
+                  style: const TextStyle(color: Colors.black),
+                ),
+              );
+            },
+            containerColor: const Color.fromRGBO(27, 27, 27, 1),
+            messagePadding: const EdgeInsets.symmetric(
+              horizontal: 12,
+              vertical: 8,
+            ),
+          //   currentUserContainerPadding: const EdgeInsets.only(
+          //     left: 48,
+          //     right: 8,
+          //   ),
+          //   otherUserContainerPadding: const EdgeInsets.only(
+          //     left: 8,
+          //     right: 48,
+          //   ),
+          ),
+        ),
       ),
     );
   }
 
   void sendMessage(ChatMessage chatMessage) {
-  setState(() {
-    messages = [chatMessage, ...messages];
-  });
-
-  try {
-    String question = chatMessage.text;
-    List<Uint8List> images = [];
-
-    if (chatMessage.medias?.isNotEmpty ?? false) {
-      images = [File(chatMessage.medias!.first.url).readAsBytesSync()];
-    }
-
-    gemini.streamGenerateContent(question, images: images).listen((event) {
-      // Extract text properly
-      String response =
-          event.content?.parts?.map((part) {
-            if (part is TextPart) return part.text;
-            return ""; // Handle non-text parts gracefully
-          }).join(" ") ?? "";
-
-      ChatMessage? lastMessage = messages.isNotEmpty ? messages.first : null;
-
-      if (lastMessage != null && lastMessage.user == geminiUser) {
-        messages.removeAt(0); // Remove from list before modifying
-        lastMessage.text += " $response";
-        setState(() {
-          messages = [lastMessage, ...messages];
-        });
-      } else {
-        ChatMessage message = ChatMessage(
-          text: response,
-          user: geminiUser,
-          createdAt: DateTime.now(),
-        );
-        setState(() {
-          messages = [message, ...messages];
-        });
-      }
+    setState(() {
+      messages = [chatMessage, ...messages];
     });
-  } catch (e) {
-    print("Error: $e");
+
+    try {
+      String question = chatMessage.text;
+      List<Uint8List> images = [];
+
+      if (chatMessage.medias?.isNotEmpty ?? false) {
+        images = [File(chatMessage.medias!.first.url).readAsBytesSync()];
+      }
+
+      gemini.streamGenerateContent(question, images: images).listen((event) {
+        // Extract text properly
+        String response = event.content?.parts
+                ?.map((part) {
+                  if (part is TextPart) return part.text;
+                  return ""; // Handle non-text parts gracefully
+                })
+                .join(" ") ??
+            "";
+
+        ChatMessage? lastMessage = messages.isNotEmpty ? messages.first : null;
+
+        if (lastMessage != null && lastMessage.user == geminiUser) {
+          messages.removeAt(0); // Remove from list before modifying
+          lastMessage.text += " $response";
+          setState(() {
+            messages = [lastMessage, ...messages];
+          });
+        } else {
+          ChatMessage message = ChatMessage(
+            text: response,
+            user: geminiUser,
+            createdAt: DateTime.now(),
+          );
+          setState(() {
+            messages = [message, ...messages];
+          });
+        }
+      });
+    } catch (e) {
+      print("Error: $e");
+    }
   }
-}
 
   voidSendMediaMessage() async {
     ImagePicker picker = ImagePicker();
